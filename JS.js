d9e0c87eb1bd86d62ed2e34a456692033aa8d4ce
lian0318 <lian0138@algonquinlive.com>
2025-03-12 09:49:13 -0400

    update

--- JS.js
+++ JS.js
@@ -62,6 +62,13 @@ class MenuButtonActions {
   setFocusToMenuitem(newMenuitem) {
     this.menuitemNodes.forEach(function (item) {
 // TOUFIC'S COMMENT: Placeholder for the roving tabindex logic  ;)
+// Lecture Page 28
+      if (item === newMenuitem) {
+        item.tabIndex = 0; 
+        newMenuitem.focus(); 
+      } else {
+        item.tabIndex = -1; 
+      }
     });
   }
 
